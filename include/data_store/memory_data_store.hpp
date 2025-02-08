08257f31641364aff63658fb5069ec6ad3c1c823
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2025-02-09 00:03:29 +0100

    update

--- include/data_store/memory_data_store.hpp
+++ include/data_store/memory_data_store.hpp
@@ -11,8 +11,10 @@ private:
 public:
   MemoryDataStore();
 
-  bool set(const std::string &key, const std::string &val);
+  bool set(const std::string &key, const std::string &val); // Add more options
   std::optional<std::string> get(const std::string &key) const;
+
+  // other methods (del, exists) later sha
 };
 
 #endif // !MEMORY_DATA_STORE_H
