92227ff968aeb8c742d0b357ba3ca264fe30044a
olamilekan-adekele <olamilekanadeleke123@gmail.com>
2025-05-25 19:54:21 +0100

    min or fix

--- include/server/server.hpp
+++ include/server/server.hpp
@@ -1,12 +1,10 @@
 #pragma once
-#ifndef SERVER_H
-#define SERVER_H
 
 #include <stdio.h>
 #include <unistd.h>
 
-#include <boost/fiber/all.hpp>
 #include <functional>
+#include <vector>
 
 #include "../network/listening_socket.hpp"
 
@@ -23,9 +21,6 @@ class Server {
   void handle_client(int client_socket);
   void accept_connections();
 
-  boost::fibers::condition_variable_any shutdown_cv;
-  boost::fibers::mutex shutdown_mutex;
-
  public:
   Server();
   ~Server() { delete socket; }
@@ -34,5 +29,3 @@ class Server {
   void start_server();
   void stop_server();
 };
-
-#endif
