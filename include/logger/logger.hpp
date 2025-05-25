e60d252f9532f6e4027459ddd77346f399248f7b
olamilekan-adekele <olamilekanadeleke123@gmail.com>
2025-05-25 19:53:50 +0100

    chore: added logger and fmt then set up vcpkg

---
+++ include/logger/logger.hpp
@@ -0,0 +1,16 @@
+#include <fmt/format.h>
+
+#include <string>
+
+class Logger {
+ public:
+  template <typename S, typename... Args>
+  static void log(const S& format_str, Args&&... args) {
+    std::string formatted_message = fmt::format(format_str, std::forward<Args>(args)...);
+    log_impl(formatted_message);
+  }
+  virtual ~Logger() = default;
+
+ private:
+  static void log_impl(const std::string& message);
+};
---
+++ src/logger/logger.cpp
@@ -0,0 +1,17 @@
+#include "../include/logger/logger.hpp"
+
+#include <fmt/base.h>
+#include <fmt/core.h>
+
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+
+void Logger::log_impl(const std::string& message) {
+  auto now = std::chrono::system_clock::now();
+  std::time_t current_time = std::chrono::system_clock::to_time_t(now);
+  std::tm* local_tm = std::gmtime(&current_time);
+
+  std::cout << "[" << std::put_time(local_tm, "%Y-%m-%d %H:%M:%S") << "] [LOG] " << message << std::endl;
+}
